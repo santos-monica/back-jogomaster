8fadc05eccf543e4ab880f60a7388ae8769f6d77
Mônica Santos <monicasantosdev@gmail.com>
2018-09-11 22:40:25 -0300

    Criação da solução das APIs do projeto

---
+++ JogoMaster/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JogoMaster.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ JogoMaster/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JogoMaster.WebApiApplication" Language="C#" %>
