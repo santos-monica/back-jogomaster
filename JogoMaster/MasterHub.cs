699f73b6beb47c28ddec3692e45a53f2fcd9160c
santos-monica <monicasantosdev@gmail.com>
2018-11-10 16:44:24 -0200

    changes

---
+++ JogoMaster/MasterHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JogoMaster
+{
+    public class MasterHub : Hub
+    {
+    }
+}
---
+++ JogoMaster/MasterMessageRelay.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace JogoMaster
+{
+    public class MasterMessageRelay
+    {
+        public MasterMessageRelay(IHubContext<MasterHub> hubContext)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+
+                }
+            });
+        }
+    }
+}
