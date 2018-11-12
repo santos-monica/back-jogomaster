bd8b141366bb2605f88b90499d131a8fe53dfa87
santos-monica <monicasantosdev@gmail.com>
2018-11-12 20:33:28 -0200

    #1 implementação de webSocket (score)

---
+++ JogoMaster/Models/Jogada.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JogoMaster.Models
+{
+    public class Jogada
+    {
+        public int IdUser { get; set; }
+        public string Username { get; set; }
+        public int IdTema { get; set; }
+        public bool Correta { get; set; }
+    }
+}
