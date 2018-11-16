8b7623b404c8083b017781a61f3c46a16241b743
santos-monica <monicasantosdev@gmail.com>
2018-11-16 14:17:14 -0200

    Primeira implementação do conceito de sala (sem integração com banco de dados)

---
+++ JogoMaster/Controllers/SalaController.cs
@@ -0,0 +1,108 @@
+﻿using JogoMaster.Models;
+using Microsoft.Web.WebSockets;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace JogoMaster.Controllers
+{
+    public class SalaController : ApiController
+    {
+        public HttpResponseMessage Get(int UsuarioId, string Username, int Tema, int Nivel, int Jogadores)
+        {
+            var dados = new CriacaoSala
+            {
+                Jogadores = Jogadores,
+                NivelId = Nivel,
+                Username = Username,
+                UsuarioId = UsuarioId,
+                TemasIds = new List<int> { Tema },
+                NovaSala = true
+            };
+
+            HttpContext.Current.AcceptWebSocketRequest(new SalaWebSocketHandler(dados));
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        }
+
+        public HttpResponseMessage Get(int UsuarioId, string Username)
+        {
+            var dados = new CriacaoSala
+            {
+                Username = Username,
+                UsuarioId = UsuarioId,
+                NovaSala = false
+            };
+
+            HttpContext.Current.AcceptWebSocketRequest(new SalaWebSocketHandler(dados));
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        }
+
+        class SalaWebSocketHandler : WebSocketHandler
+        {
+            private static WebSocketCollection _salaClients = new WebSocketCollection();
+            private List<int> UsuarioIds = new List<int>();
+            private List<string> Usernames = new List<string>();
+            private int NivelId;
+            private List<int> TemasIds = new List<int>();
+            private int MaximoJogadores;
+            private int JogadoresNaSala = 0;
+            private int UsuarioCriador;
+
+            private Sala SalaPartida = new Sala();
+
+            public SalaWebSocketHandler(CriacaoSala dados)
+            {
+                if (dados.NovaSala)
+                {
+                    UsuarioCriador = dados.UsuarioId;
+                    UsuarioIds.Add(dados.UsuarioId);
+                    Usernames.Add(dados.Username);
+                    NivelId = dados.NivelId;
+                    TemasIds = dados.TemasIds;
+                    MaximoJogadores = dados.Jogadores;
+                    JogadoresNaSala++;
+
+                    SalaPartida.Nivel = dados.NivelId;
+                    SalaPartida.Temas = dados.TemasIds;
+                    SalaPartida.TotalJogadores = dados.Jogadores;
+                }
+                else
+                {
+                    JogadoresNaSala++;
+                    Usernames.Add(dados.Username);
+                    UsuarioIds.Add(dados.UsuarioId);
+                }
+
+                SalaPartida.JogadoresNaSala = JogadoresNaSala;
+                SalaPartida.Usernames = Usernames;
+                SalaPartida.Usuarios = UsuarioIds;
+            }
+
+            public override void OnOpen()
+            {
+                _salaClients.Add(this);
+            }
+
+            public override void OnMessage(string teste)
+            {
+                var serializer = new JavaScriptSerializer();
+                var novoJogador = serializer.Deserialize<ParticiparSala>(teste);
+
+                SalaPartida.Usernames.Add(novoJogador.Username);
+                SalaPartida.Usuarios.Add(novoJogador.UsuarioId);
+                SalaPartida.JogadoresNaSala++;
+
+                if (SalaPartida.JogadoresNaSala == SalaPartida.TotalJogadores)
+                {
+                    SalaPartida.SalaCheia = true;
+                }
+
+                var salaAtualizada = serializer.Serialize(SalaPartida);
+                _salaClients.Broadcast(salaAtualizada);
+            }
+        }
+    }
+}
---
+++ JogoMaster/Models/Sala.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JogoMaster.Models
+{
+    public class CriacaoSala
+    {
+        public int UsuarioId { get; set; }
+        public string Username { get; set; }
+        public int NivelId { get; set; }
+        public List<int> TemasIds { get; set; }
+        public int Jogadores { get; set; }
+        public bool NovaSala { get; set; }
+
+        public CriacaoSala()
+        {
+            TemasIds = new List<int>();
+        }
+    }
+
+    public class ParticiparSala
+    {
+        public int UsuarioId { get; set; }
+        public string Username { get; set; }
+    }
+
+    public class Sala
+    {
+        public int SalaId { get; set; }
+        public List<int> Usuarios { get; set; }
+        public List<string> Usernames { get; set; }
+        public int TotalJogadores { get; set; }
+        public int JogadoresNaSala { get; set; }
+        public List<int> Temas { get; set; }
+        public int Nivel { get; set; }
+        public bool SalaCheia { get; set; }
+
+        public Sala()
+        {
+            Usuarios = new List<int>();
+            Usernames = new List<string>();
+            Temas = new List<int>();
+            SalaCheia = false;
+        }
+    }
+
+}
